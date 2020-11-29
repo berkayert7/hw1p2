db8497f34c8497ba4b98314e3892f40eec87d51f
Berkay ERTURK <berturk@etu.edu.tr>
2020-11-29 16:22:55 +0300

    Set the port number from the environment variable $PORT

--- src/main/java/hw1p2/App.java
+++ src/main/java/hw1p2/App.java
@@ -22,45 +22,52 @@ public class App {
     }
 
     public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+      Logger logger = LogManager.getLogger(App.class);
 
-        get("/", (req, res) -> "Hello, World");
-
-        post("/compute", (req, res) -> {
-          //System.out.println(req.queryParams("input1"));
-          //System.out.println(req.queryParams("input2"));
-
-          String input1 = req.queryParams("input1");
-          java.util.Scanner sc1 = new java.util.Scanner(input1);
-          sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext())
-          {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
-          }
-          sc1.close();
-          System.out.println(inputList);
+      int port = Integer.parseInt(System.getenv("PORT"));
+      port(port);
+      logger.error("Current port number:" + port);
 
 
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+      //port(getHerokuAssignedPort());
 
-          boolean result = App.search(inputList, input2AsInt);
+      get("/", (req, res) -> "Hello, World");
 
-          Map<String, Boolean> map = new HashMap<String, Boolean>();
-          map.put("result", result);
-          return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());
+      post("/compute", (req, res) -> {
+        //System.out.println(req.queryParams("input1"));
+        //System.out.println(req.queryParams("input2"));
+
+        String input1 = req.queryParams("input1");
+        java.util.Scanner sc1 = new java.util.Scanner(input1);
+        sc1.useDelimiter("[;\r\n]+");
+        java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+        while (sc1.hasNext())
+        {
+          int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+          inputList.add(value);
+        }
+        sc1.close();
+        System.out.println(inputList);
 
 
-        get("/compute",
-            (rq, rs) -> {
-              Map<String, String> map = new HashMap<String, String>();
-              map.put("result", "not computed yet!");
-              return new ModelAndView(map, "compute.mustache");
-            },
-            new MustacheTemplateEngine());
+        String input2 = req.queryParams("input2").replaceAll("\\s","");
+        int input2AsInt = Integer.parseInt(input2);
+
+        boolean result = App.search(inputList, input2AsInt);
+
+        Map<String, Boolean> map = new HashMap<String, Boolean>();
+        map.put("result", result);
+        return new ModelAndView(map, "compute.mustache");
+      }, new MustacheTemplateEngine());
+
+
+      get("/compute",
+          (rq, rs) -> {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("result", "not computed yet!");
+            return new ModelAndView(map, "compute.mustache");
+          },
+          new MustacheTemplateEngine());
     }
 
     static int getHerokuAssignedPort() {
